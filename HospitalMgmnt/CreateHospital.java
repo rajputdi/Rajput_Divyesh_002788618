d23d6400a1c4e05c1665e1668ec23eef14a5d7d7
rajputdi <114702639+rajputdi@users.noreply.github.com>
2022-10-21 19:05:53 -0400

    commit-10/21-initiating lab 2

---
+++ HospitalMgmnt/CreateHospital.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hospital.hospitalmgmnt;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Divyesh Rajput
+ */
+public class CreateHospital implements Serializable {
+    private String hospitalID;
+    private String hospitalName;
+    private String streetAddress;   
+
+    public String getHospitalID() {
+        return hospitalID;
+    }
+
+    public void setHospitalID(String hospitalID) {
+        this.hospitalID = hospitalID;
+    }
+
+    public String getHospitalName() {
+        return hospitalName;
+    }
+
+    public void setHospitalName(String hospitalName) {
+        this.hospitalName = hospitalName;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+    
+    
+}
